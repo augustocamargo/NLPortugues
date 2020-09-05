c053b371c554001223c929ef4caf9e7dc1dc7aa6
Augusto Camargo <sp_augusto@msn.com>
2020-09-04 22:03:10 -0300

    model ok

--- Semana 03/pruebas.py
+++ Semana 03/pruebas.py
@@ -18,7 +18,7 @@ import numpy as np
 from unidecode import unidecode
 import re
 spacyPT = spacy.load('pt_core_news_lg')
-b2wCorpus = pd.read_csv("/home/augusto/Documents/GitHub/NLPortugues/Semana 03/data/b2w-10k.csv", encoding='utf-8')
+b2wCorpus = pd.read_csv("/home/augusto/Documents/GitHub/NLPortugues/Semana 03/data/b2w-10k.csv", encoding='utf-8',nrows=200)
 b2wCorpus= b2wCorpus[["review_text", "recommend_to_a_friend"]]
 b2wCorpus['recommend_to_a_friend'].replace({'No': 0, 'Yes': 1}, inplace = True)
 print(b2wCorpus.head())
@@ -33,6 +33,39 @@ b2wCorpus['review_text_lema'] = b2wCorpus.review_text.apply(lambda text:
 for i, row in b2wCorpus.iterrows():
     ifor_val =   ' '.join((row['review_text_lema']).split())
     b2wCorpus.at[i,'review_text_lema']= ifor_val
-    
+
 print(b2wCorpus.head())
+
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from tensorflow.keras import layers
+from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
+
+X = b2wCorpus[['review_text_lema']]
+y = b2wCorpus[['recommend_to_a_friend']] 
+RANDOM_SEED = 42
+np.random.seed(RANDOM_SEED)
+x_train, x_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=RANDOM_SEED)
+
+textVecLayer = TextVectorization(output_mode="int", pad_to_max_tokens=True)
+textVecLayer.adapt(X.to_numpy())
+vocab_size = len(textVecLayer.get_vocabulary())
+model = tf.keras.Sequential([
+    ############ Seu código aqui##################
+    tf.keras.Input(shape=(1, ),
+                   dtype=tf.string),
+    textVecLayer,
+    tf.keras.layers.Embedding(input_dim=vocab_size,
+                              output_dim = 32),
+    ##############################################
+    # Conv1D + global max pooling
+    layers.Conv1D(128, 7, padding='valid', activation='relu', strides=3),
+    layers.Conv1D(128, 7, padding='valid', activation='relu', strides=3),
+    layers.GlobalMaxPooling1D(),
+
+    layers.Dense(128, activation='relu'),
+    layers.Dropout(0.5),
+    layers.Dense(1, activation='sigmoid'),
+])
+
 
