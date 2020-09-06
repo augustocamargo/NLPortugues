f1e5d21d9501d1c21061e9bb5f16fcaa8f471ffb
Augusto Camargo <sp_augusto@msn.com>
2020-09-05 22:20:02 -0300

    rodou a loss

--- Semana 03/pruebas.py
+++ Semana 03/pruebas.py
@@ -17,12 +17,13 @@ import pandas as pd
 import numpy as np
 from unidecode import unidecode
 import re
+import sys
 spacyPT = spacy.load('pt_core_news_lg')
-b2wCorpus = pd.read_csv("/home/augusto/Documents/GitHub/NLPortugues/Semana 03/data/b2w-10k.csv", encoding='utf-8',nrows=2000)
-#b2wCorpus = pd.read_csv("C:\\tmp\\b2w-10k.csv", encoding='utf-8',nrows=200)
+#b2wCorpus = pd.read_csv("/home/augusto/Documents/GitHub/NLPortugues/Semana 03/data/b2w-10k.csv", encoding='utf-8',nrows=2000)
+b2wCorpus = pd.read_csv("C:\\tmp\\b2w-10k.csv", encoding='utf-8',nrows=20)
 b2wCorpus= b2wCorpus[["review_text", "recommend_to_a_friend"]]
 b2wCorpus['recommend_to_a_friend'].replace({'No': 0, 'Yes': 1}, inplace = True)
-print(b2wCorpus.head())
+
 for i, row in b2wCorpus.iterrows():
     ifor_val = unidecode(row['review_text']).lower()
     b2wCorpus.at[i,'review_text']= ifor_val
@@ -35,7 +36,6 @@ for i, row in b2wCorpus.iterrows():
     ifor_val =   ' '.join((row['review_text_lema']).split())
     b2wCorpus.at[i,'review_text_lema']= ifor_val
 
-print(b2wCorpus.head())
 
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
@@ -45,17 +45,17 @@ from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
 
 X = b2wCorpus[['review_text']]
 y = b2wCorpus[['recommend_to_a_friend']] 
-print("\n------\n")
-print(b2wCorpus.shape)
-print("\n------\n")
 
 RANDOM_SEED = 42
 np.random.seed(RANDOM_SEED)
 x_train, x_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=RANDOM_SEED)
 
-textVecLayer = TextVectorization(output_mode="int", pad_to_max_tokens=True)
+textVecLayer = TextVectorization( output_mode="int", pad_to_max_tokens=True)
 textVecLayer.adapt(X.to_numpy())
 vocab_size = len(textVecLayer.get_vocabulary())
+
+#sys.exit()
+
 model = tf.keras.Sequential([
     ############ Seu código aqui##################
     tf.keras.Input(shape=(1, ),
@@ -77,6 +77,6 @@ model = tf.keras.Sequential([
 model.compile(
     loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=400, batch_size=10,validation_data=(x_val, y_val))    
+model.fit(x = x_train, y = y_train, epochs=400, batch_size=10,validation_data=(x_val, y_val))    
 
 
