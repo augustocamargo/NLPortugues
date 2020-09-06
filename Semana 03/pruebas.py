89e933db653979ef7dbc783b00c989c285b31d21
Augusto Camargo <sp_augusto@msn.com>
2020-09-06 14:21:14 -0300

    limpo mais nao funciona com +50 registros

--- Semana 03/pruebas.py
+++ Semana 03/pruebas.py
@@ -1,64 +1,41 @@
 #! /bin/python3
-#import nltk
-#from nltk.stem import WordNetLemmatizer
-#from nltk.corpus import floresta
-##stemmer = nltk.stem.RSLPStemmer()
-#wordwet_lemmatizer = WordNetLemmatizer()
-#words = ['recomendo', 'gostei', 'péssimo', 'ruim','odiei', 'amei', 'ruim', 'ótimo']
-#for word in words:
-    #lemma2 = wordwet_lemmatizer.lemmatize(word)
- #   lemma2 = stemmer.stem(word)
-    #print(lemma)
-    #print("\n")
-    #print(lemma2)
-
-import spacy
 import pandas as pd
 import numpy as np
 from unidecode import unidecode
-import re
-import sys
-spacyPT = spacy.load('pt_core_news_lg')
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from tensorflow.keras import layers
+from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
+
 #b2wCorpus = pd.read_csv("/home/augusto/Documents/GitHub/NLPortugues/Semana 03/data/b2w-10k.csv", encoding='utf-8',nrows=2000)
-b2wCorpus = pd.read_csv("C:\\tmp\\b2w-10k.csv", encoding='utf-8',nrows=1000)
+b2wCorpus = pd.read_csv("C:\\tmp\\b2w-10k.csv", encoding='utf-8',nrows=50)
 b2wCorpus= b2wCorpus[["review_text", "recommend_to_a_friend"]]
 b2wCorpus['recommend_to_a_friend'].replace({'No': 0, 'Yes': 1}, inplace = True)
 
 for i, row in b2wCorpus.iterrows():
     ifor_val = unidecode(row['review_text']).lower()
     b2wCorpus.at[i,'review_text']= ifor_val
-#print(b2wCorpus.head())
-#print(spacyPT.Defaults.stop_words)
-#b2wCorpus['review_text_lema'] = b2wCorpus.review_text.apply(lambda text: 
-#                                          " ".join(re.sub('[^A-Za-z0-9]+', ' ', token.lemma_) for token in spacyPT(text) 
-#                                                   if not token.is_stop and token.pos_ not in ['PROPN','NUM','SYM'] ))
-#for i, row in b2wCorpus.iterrows():
-#    ifor_val =   ' '.join((row['review_text_lema']).split())
-#    b2wCorpus.at[i,'review_text_lema']= ifor_val
 
-
-import tensorflow as tf
-from sklearn.model_selection import train_test_split
-from tensorflow.keras import layers
-from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
-
-
-X = b2wCorpus[['review_text']]
+x = b2wCorpus[['review_text']]
 y = b2wCorpus[['recommend_to_a_friend']] 
+
+print(b2wCorpus.recommend_to_a_friend.value_counts()/ b2wCorpus.shape[0])
 
 RANDOM_SEED = 42
 np.random.seed(RANDOM_SEED)
-x_train, x_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=RANDOM_SEED)
+x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.2, random_state=RANDOM_SEED)
 
 #textVecLayer = TextVectorization( output_mode="int", pad_to_max_tokens=True)
+text_dataset = tf.data.Dataset.from_tensor_slices(x.to_numpy())
 vectorize_layer = TextVectorization(
                                         max_tokens=5000,
+                                        standardize='lower_and_strip_punctuation',
+                                        split='whitespace',
                                         output_mode='int',
-                                        output_sequence_length=4,
                                         pad_to_max_tokens=True
                                         )
 
-vectorize_layer.adapt(X.to_numpy())
+vectorize_layer.adapt(text_dataset.batch(64))
 vocab_size = len(vectorize_layer.get_vocabulary())
 
 #sys.exit()
@@ -83,7 +60,15 @@ model = tf.keras.Sequential([
 
 model.compile(
     loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-model.fit(x = x_train, y = y_train, epochs=5, batch_size=10,validation_data=(x_val, y_val))    
-#p = model.predict( ['eu odiei este produto'])
+with tf.device("gpu:0"):
+   model.fit(x_train,  y_train, epochs=5, batch_size=10,validation_data=(x_val, y_val))    
+   score = model.evaluate(x_val, y_val, verbose=1)
+   print(score)
+   x_v = x_val.to_numpy()
+   x_v = np.append(x_v,['eu odei o produto'])
+   x_v = np.append(x_v,['eu gostei o produto'])
+   x_v = np.append(x_v,['náo comparei de novo'])
+   print(x_v)
+   pred = model.predict(x_v)
+   print(np.round(pred))
 
